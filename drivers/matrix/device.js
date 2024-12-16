67fb30c9015a23713720860364791134fb95efb1
meg768 <magnus@egelberg.se>
2024-12-16 19:58:26 +0100

    -

--- drivers/matrix/device.js
+++ drivers/matrix/device.js
@@ -4,86 +4,24 @@ const { Device } = require('homey');
 
 class MyDevice extends Device {
 	async onInit() {
-		this.conditions = [];
-		this.actions = [];
 		this.debug = this.log;
         this.app = this.homey.app;
         this.mqtt = this.app.mqtt;
+	}
 
-        this.addAction('text', async (args, state) => {
-            let {message} = args;
-
-            let payload = {};
-            payload.animation = 'text';
-            payload.text = message;
-            payload.textColor = 'red';
-
-            this.log(payload);
-
-            await this.mqtt.publish(`${this.getSetting('mqtt').topic}`, JSON.stringify(payload), {retain:true});
-
-        });
-
-        this.addAction('text-with-color', async (args, state) => {
-            let {message, color} = args;
-
-            let payload = {};
-            payload.animation = 'text';
-            payload.text = message;
-            payload.textColor = color;
-
-            this.log(payload);
-
-            await this.mqtt.publish(`${this.getSetting('mqtt').topic}`, JSON.stringify(payload), {retain:true});
-
-        });        
+	async publish(payload) {
+		this.log(payload);
+		await this.mqtt.publish(`${this.getSetting('mqtt').topic}`, JSON.stringify(payload), { retain: true });
 	}
 
 	async onUninit() {
-		// Cleanup conditions
-		if (this.conditions) {
-			this.conditions.forEach((condition) => {
-				condition.removeAllListeners();
-			});
-		}
-
-		// Cleanup actions
-		if (this.actions) {
-			this.actions.forEach((action) => {
-				action.removeAllListeners();
-			});
-		}
-
 		await this.homey.app.unregisterDevice(this);
-	}
-
-	async addCondition(name, fn) {
-		let condition = this.homey.flow.getConditionCard(name);
-		condition.registerRunListener(fn);
-
-		this.conditions.push(condition);
-	}
-
-	async addAction(name, fn) {
-		let action = this.homey.flow.getActionCard(name);
-		action.registerRunListener(fn);
-
-		this.actions.push(action);
-	}
-
-	async trigger(name, args) {
-		if (args) {
-			this.log(`Triggering '${name}' with parameters ${JSON.stringify(args)}`);
-		} else {
-			this.log(`Triggering '${name}'}`);
-		}
-		const triggerCard = this.homey.flow.getDeviceTriggerCard(name);
-		await triggerCard.trigger(this, args);
 	}
 
 	async onSettings({ oldSettings, newSettings, changedKeys }) {
 		this.log('MyDevice settings where changed');
 	}
+		
 
 
 }
--- drivers/matrix/driver.js
+++ drivers/matrix/driver.js
@@ -7,9 +7,44 @@ class MyDriver extends Driver {
   /**
    * onInit is called when the driver is initialized.
    */
-  async onInit() {
-    this.log('MyDriver has been initialized');
-  }
+    async onInit() {
+        this.log('MyDriver has been initialized');
+        
+        this.addAction('text', async ({device, message}) => {
+
+            let payload = {};
+            payload.animation = 'text';
+            payload.text = message;
+            payload.textColor = 'red';
+
+            await device.publish(payload);
+        });
+
+        this.addAction('text-with-color', async ({ device, message, color }) => {
+
+            let payload = {};
+            payload.animation = 'text';
+            payload.text = message;
+            payload.textColor = color;
+
+            await device.publish(payload);
+        });
+
+        this.addAction('random-gif', async ({ device, duration }) => {
+
+            let payload = {};
+            payload.animation = 'gif';
+            payload.duration = duration * 1000
+
+            await device.publish(payload);
+        });
+        
+    }
+
+    async addAction(name, fn) {
+        let action = this.homey.flow.getActionCard(name);
+        action.registerRunListener(fn);
+    }
 
   /**
    * onPairListDevices is called when a user is adding a device
@@ -21,7 +56,7 @@ class MyDriver extends Driver {
         {
             name: 'Matrix 32x32',
             data: {
-                id: 'pushover',
+                id: 'matrix-32x32',
             },
             settings: {
                 mqtt: {
@@ -32,14 +67,37 @@ class MyDriver extends Driver {
         {
             name: 'Matrix 64x32',
             data: {
-                id: 'pushover',
+                id: 'matrix-64x32',
             },
             settings: {
                 mqtt: {
                     topic:'matrix/64x32'
                 }
             }
+        },
+        {
+            name: 'Matrix 64x64',
+            data: {
+                id: 'matrix-64x64',
+            },
+            settings: {
+                mqtt: {
+                    topic: 'matrix/64x64'
+                }
+            }
+        },
+        {
+            name: 'Matrix 96x96',
+            data: {
+                id: 'matrix-96x96',
+            },
+            settings: {
+                mqtt: {
+                    topic: 'matrix/96x96'
+                }
+            }
         }
+
     ];
   }
 
